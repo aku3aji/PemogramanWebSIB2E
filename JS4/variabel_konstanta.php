ef0f37a206f852fe7d2f14dd2c4174a188b4952e
aku3aji <triaji3ibnu@gmail.com>
2024-03-08 15:06:02 +0700

    first commit

---
+++ JS4/variabel_konstanta.php
@@ -0,0 +1,9 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+$benar = true;
+$salah = false;
+echo "Variable benar: $benar, "
+?>
